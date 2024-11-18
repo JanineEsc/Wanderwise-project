5d2bd52ea9fa5803882a20d277da2bb51de56aa8
Janine Escobillo <janineescobillo@hotmail.com>
2024-11-18 14:55:18 +0100

    adds header, searchmobile and footer

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,18 @@
-
+import { Footer } from "./_components/Footer"
+import { Header } from "./_components/header"
 import { Listing_card } from "./_components/listing_card"
 import { NavbarMobile } from "./_components/NavbarMobile"
 import { SearchMobile } from "./_components/SearchMobile"
 
 const LandingPage = () => {
+
+
   return (
-    <main className="bg-timberwolf min-h-screen">
-      <div className="flex items-center justify-center">
-      </div>
+    <main className="bg-timberwolf min-h-screen pb-24">
+      <Header />
+      <SearchMobile />
       <Listing_card />
+      <Footer />
       <NavbarMobile />
     </main>
   )
