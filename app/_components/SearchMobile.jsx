7def37f8715cb34344d6d866e0177d69b2b9fe66
Janine Escobillo <janineescobillo@hotmail.com>
2024-11-18 15:03:49 +0100

    adds filterIsOpen state and functionality and styling

--- app/_components/SearchMobile.jsx
+++ app/_components/SearchMobile.jsx
@@ -1,11 +1,51 @@
-import { Search } from "lucide-react";
+'use client'
+
+import { Search, Settings2 } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { Filter } from "./Filter";
 
 export const SearchMobile = () => {
+  const [filterIsOpen, setFilterIsOpen] = useState(false)
+  const filterRef = useRef(null);
+
+
+  const handleSettingClick = () => {
+    setFilterIsOpen(!filterIsOpen)
+  }
+
+  const handleClickOutside = (event) => {
+    if (filterRef.current && !filterRef.current.contains(event.target)) {
+      setFilterIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+
   return (
-    <button className="bg-BrunswickGreen flex items-center gap-8 w-3/4 p-2 rounded-md just">
-      <Search />
-      <span className="mr-2">Search...</span>
-    </button>
-    
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex justify-center items-center mt-1 w-full gap-2">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="bg-BrunswickGreen placeholder-timberwolf flex items-center gap-8 py-2 w-1/2 px-3 rounded-3xl"
+        >
+        </input>
+        <Settings2
+          onClick={handleSettingClick}
+          className="text-BrunswickGreen w-8 h-8 cursor-pointer"
+        />
+      </div>
+      <div className="flex justify-center items-center mt-1 w-full gap-2" ref={filterRef}>
+        {filterIsOpen && (
+          <Filter />
+        )}
+      </div>
+    </div>
   );
 }
