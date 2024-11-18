7358317691e9e2f91dd55c7f2d11d9c67f846302
Janine Escobillo <janineescobillo@hotmail.com>
2024-11-18 14:57:56 +0100

    creates footer component

---
+++ app/_components/Footer.jsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+  return (
+    <div className="flex-col text-center fixed bottom-0 w-full bg-gray-700 hidden md:flex text-timberwolf text-sm p-3">
+      <p>Wanderwise</p>
+      <p>© 2021</p>
+    </div>
+  )
+}
