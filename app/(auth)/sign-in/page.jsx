5e98a08b5b94f782ebc03932443ae0333df04f01
Janine Escobillo <janineescobillo@hotmail.com>
2024-11-18 14:56:50 +0100

    adds login, navbarmobile and footer comps to sign in page

--- app/(auth)/sign-in/page.jsx
+++ app/(auth)/sign-in/page.jsx
@@ -1,8 +1,15 @@
+import { NavbarMobile } from '@/app/_components/NavbarMobile';
 import React from 'react';
+import { LogIn } from '../_components/LogIn';
+import { Footer } from '@/app/_components/Footer';
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <main className='bg-timberwolf min-h-screen w-full'>
+      <LogIn />
+      <NavbarMobile />
+      <Footer />
+    </main>
   );
 };
 
