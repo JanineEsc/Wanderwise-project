6ec0b4480ec7b1affd381c9e6b0d57d5845fe622
Janine Escobillo <janineescobillo@hotmail.com>
2024-11-18 15:04:33 +0100

    adds routes to "/"

--- app/searchPage/_components/SearchButton.jsx
+++ app/searchPage/_components/SearchButton.jsx
@@ -1,9 +1,16 @@
+import { useRouter } from "next/navigation";
+
 export const SearchButton = () => {
+  const router = useRouter();
+
+  const handleSearch = () => {
+    // Navigate to the home page
+    router.push("/");
+  };
+
   return (
-    <div className="flex justify-end">
-      <button className="bg-BrunswickGreen p-1 rounded-md w-1/2">
-        Search
-      </button>
-    </div>
-  )
-}
+    <button className="bg-BrunswickGreen p-1 rounded-md w-1/5" onClick={handleSearch}>
+      <p className="text-center text-timberwolf">Search</p>
+    </button>
+  );
+};
